5b6f5a9719f92df5e2a387c67f9f26ba73a3f0d3
Cristiano A Souza Costa Filho <cristiano.costa@doalti.corp>
2025-04-17 14:11:05 -0300

    Leitura de texto ta pronta!

---
+++ src/textprint.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class textprint {
+
+
+    public String path;
+    public File text;
+
+    public void imprime_texto(){
+
+        File text = new File(path);
+        Scanner sc = null;
+
+        try {
+
+            sc = new Scanner(text);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error :" + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+
+        }
+
+
+
+    }
+
+
+
+
+
+}
